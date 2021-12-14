a565a0830e1c5a32be58d24d34560d7b3c63b8ce
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2021-12-15 01:05:01 +0200

    Added suggestion comments.

--- uri-parse.pl
+++ uri-parse.pl
@@ -11,9 +11,14 @@ uri_parse(String, uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment)) :-
     portMachine(UserhostLeftover, Port, PortLeftover),
     pathMachine(PortLeftover, Path, PathLeftover),
     queryMachine(PathLeftover, Query, QueryLeftover),
-    fragmentMachine(QueryLeftover, Fragment, "").
+    fragmentMachine(QueryLeftover, Fragment).
 
 % uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment)
+
+% Per l'opzionalità, i caratteri separatori vengono consumati troppe volte.
+% Costruire macchina per Path Query e Fragment.
+% Port può essere presente solo con // iniziali.
+% Dopo host e userinfo può esserci qualcosa solo con // iniziali.
 
 % consult('uri-parse.pl').
 % uri_parse("whatever://maria.z@prova.host.dai:77", Uri).
