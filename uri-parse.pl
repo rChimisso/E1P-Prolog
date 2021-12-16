baae88dbbec0093ee27ca8f92e98862624371c63
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2021-12-16 16:28:49 +0200

    Added uri_display/1 and uri_display/2.

--- uri-parse.pl
+++ uri-parse.pl
@@ -5,3 +5,11 @@ uri_parse(String, uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment)) :-
 	string_chars(String, Chars),
     schemeMachine(Chars, Scheme, SchemeLeftover),
 	uriMachine(SchemeLeftover, Userinfo, Host, Port, Path, Query, Fragment).
+
+uri_display(uri(Scheme, Userinfo, Host, Port, Path, Query, Frag), Stream) :-
+	write(Stream, uri(Scheme, Userinfo, Host, Port, Path, Query, Frag)),
+	nl(Stream).
+
+uri_display(uri(Scheme, Userinfo, Host, Port, Path, Query, Frag)) :-
+	current_output(Stream),
+	uri_display(uri(Scheme, Userinfo, Host, Port, Path, Query, Frag), Stream).
