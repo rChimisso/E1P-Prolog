ebbf7dd788d8d6c8c7e0b28e033b0a4031182c76
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2021-12-17 23:01:45 +0200

    Code refractor, added comment for known bug.

--- uri-parse.pl
+++ uri-parse.pl
@@ -1,7 +1,4 @@
 :- use_module(schemeMachine).
-:- use_module(userhostMachine).
-:- use_module(pqfZosMachine).
-:- use_module(portMachine).
 :- use_module(uriMachine).
 
 /**
@@ -9,49 +6,14 @@
  * 
  * True when the given string represents a valid URI.
  */
-uri_parse(String, uri(mailto, Userinfo, Host, '80', [], [], [])) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, mailto, SchemeLeftover),
-	!,
-	userhostMachine(SchemeLeftover, Userinfo, Host, []),
-	Userinfo \= [].
-uri_parse(String, uri(news, [], Host, '80', [], [], [])) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, news, SchemeLeftover),
-	!,
-	userhostMachine(SchemeLeftover, '', Host, []),
-	!, % Avoid considering Host as Userinfo.
-	Host \= [].
-uri_parse(String, uri(tel, Userinfo, [], '80', [], [], [])) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, tel, SchemeLeftover),
-	!,
-	userhostMachine(SchemeLeftover, Userinfo, '', []),
-	Userinfo \= [].
-uri_parse(String, uri(fax, Userinfo, [], '80', [], [], [])) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, fax, SchemeLeftover),
-	!,
-	userhostMachine(SchemeLeftover, Userinfo, '', []),
-	Userinfo \= [].
-uri_parse(String, uri(zos, Userinfo, Host, Port, Path, Query, Fragment)) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, zos, SchemeLeftover),
-	append([/, /], TrimmedChars, SchemeLeftover),
-	!, % If it starts with "//", this becomes the only possible zos production.
-    userhostMachine(TrimmedChars, Userinfo, Host, UserhostLeftover),
-	!, % Avoid considering Host as Userinfo.
-    portMachine(UserhostLeftover, Port, PortLeftover),
-    pqfZosMachine(PortLeftover, Path, Query, Fragment).
-uri_parse(String, uri(zos, [], [], '80', Path, Query, Fragment)) :-
-	string_chars(String, Chars),
-    schemeMachine(Chars, zos, SchemeLeftover),
-	!,
-	pqfZosMachine(SchemeLeftover, Path, Query, Fragment).
 uri_parse(String, uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment)) :-
 	string_chars(String, Chars),
-    schemeMachine(Chars, Scheme, SchemeLeftover),
-	uriMachine(SchemeLeftover, Userinfo, Host, Port, Path, Query, Fragment).
+    schemeMachine(Chars, Scheme, Leftover),
+	downcase_atom(Scheme, LoweredScheme),
+	uriMachine(
+		Leftover,
+		uri(LoweredScheme, Userinfo, Host, Port, Path, Query, Fragment)
+	).
 /**
  * uri_display(+Uri:uri, ++Stream:stream) is det.
  * 
--- uriMachine.pl
+++ uriMachine.pl
@@ -1,11 +1,40 @@
-:- module(uriMachine, [uriMachine/7]).
+:- module(uriMachine, [uriMachine/2]).
 
 :- use_module(schemeMachine).
 :- use_module(userhostMachine).
 :- use_module(portMachine).
 :- use_module(pqfMachine).
+:- use_module(zpqfMachine).
 
-uriMachine(Chars, Userinfo, Host, Port, Path, Query, Fragment) :-
+uriMachine(Chars, uri(mailto, Userinfo, Host, '80', [], [], [])) :-
+	!,
+    userhostMachine(Chars, Userinfo, Host, []),
+	Userinfo \= [].
+uriMachine(Chars, uri(news, [], Host, '80', [], [], [])) :-
+	!,
+    userhostMachine(Chars, '', Host, []),
+	!, % Avoid considering Host as Userinfo.
+	Host \= [].
+uriMachine(Chars, uri(tel, Userinfo, [], '80', [], [], [])) :-
+	!,
+    userhostMachine(Chars, Userinfo, '', []),
+	Userinfo \= [].
+uriMachine(Chars, uri(fax, Userinfo, [], '80', [], [], [])) :-
+	!,
+    userhostMachine(Chars, Userinfo, '', []),
+	Userinfo \= [].
+uriMachine(Chars, uri(zos, Userinfo, Host, Port, Path, Query, Fragment)) :-
+	!,
+	append([/, /], TrimmedChars, Chars),
+	!, % If it starts with "//", this becomes the only possible production.
+    userhostMachine(TrimmedChars, Userinfo, Host, UserhostLeftover),
+	!, % Avoid considering Host as Userinfo.
+    portMachine(UserhostLeftover, Port, PortLeftover),
+    zpqfMachine(PortLeftover, Path, Query, Fragment).
+uriMachine(Chars, uri(zos, [], [], '80', Path, Query, Fragment)) :-
+	!,
+    zpqfMachine(Chars, Path, Query, Fragment).
+uriMachine(Chars, uri(_, Userinfo, Host, Port, Path, Query, Fragment)) :-
 	append([/, /], TrimmedChars, Chars),
 	!, % If it starts with "//", this becomes the only possible production.
     userhostMachine(TrimmedChars, Userinfo, Host, UserhostLeftover),
@@ -16,13 +45,20 @@ uriMachine(Chars, Userinfo, Host, Port, Path, Query, Fragment) :-
  * The following version of this machine is needed only to not have a useless
  * false as a result when the URI follows the mailto scheme-syntax with
  * both Userinfo and Host present (when '@' is present).
+ * Without it, it would be checked if the URI has a path and thus the false.
  */
-uriMachine(Chars, Userinfo, Host, '80', [], [], []) :-
+uriMachine(Chars, uri(_, Userinfo, Host, '80', [], [], [])) :-
     userhostMachine(Chars, Userinfo, Host, []),
 	Userinfo \= [],
 	Host \= [],
 	!.
-uriMachine(Chars, Userinfo, Host, '80', [], [], []) :-
+uriMachine(Chars, uri(_, Userinfo, Host, '80', [], [], [])) :-
     userhostMachine(Chars, Userinfo, Host, []).
-uriMachine(Chars, [], [], '80', Path, Query, Fragment) :-
+uriMachine(Chars, uri(_, [], [], '80', Path, Query, Fragment)) :-
     pqfMachine(Chars, Path, Query, Fragment).
+
+% Due to the cut and the double productions for zos, only the production with
+% authority is considered for zos.
+% Possible solution may be to extract append, userhostMachine and portMachine
+% into a separate authorityMachine, unify then the productions (zos and generic)
+% with pqf machines.
